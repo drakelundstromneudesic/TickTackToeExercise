f03239c663b5bad7e53872703ec37f56dde7f1c1
Drake Lundstrom <Drake.Lundstrom@neudesic.com>
2023-08-02 17:01:01 -0400

    removing button changing logic

--- ljg0t8/src/App.js
+++ ljg0t8/src/App.js
@@ -1,25 +1,46 @@
+import { useState } from "react";
+
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return  (
     <>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-  </>
+      <div className="board-row">
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
+    </>
   );
 }
 
-function Square({ value }) {
-  return <button className="square">value</button>;
+function Square({value}) {
+  // const [value, setValue] = useState(null);
+  // function handleClick() {
+  //   if (value==null) {
+  //     setValue('X');
+  //   } else if (value=='X') {
+  //     setValue('O')
+  //   } else {
+  //     setValue(null)
+  //   }
+  // }
+
+  return (
+    <button
+      className="square"
+      // onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
