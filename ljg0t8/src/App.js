56825d99c523f9230901922c53c134a51d40c1ab
Drake Lundstrom <Drake.Lundstrom@neudesic.com>
2023-08-02 17:31:13 -0400

    added tile change logic

--- ljg0t8/src/App.js
+++ ljg0t8/src/App.js
@@ -2,44 +2,48 @@ import { useState } from "react";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  return  (
+  function handleClick(i) {
+    let newValue = null
+    if (squares[i]==null) {
+      newValue=('X');
+    } else if (squares[i]=='X') {
+      newValue=('O')
+    } else {
+      newValue=(null)
+    }
+    const nextSquares = [
+      ...squares.slice(0 , i),
+      newValue,
+      ...squares.slice(i+1, 9),
+    ];
+    setSquares(nextSquares);
+  }
+  return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() =>handleClick(0)}  />
+        <Square value={squares[1]} onSquareClick={() =>handleClick(1)}  />
+        <Square value={squares[2]} onSquareClick={() =>handleClick(2)}  />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() =>handleClick(3)}  />
+        <Square value={squares[4]} onSquareClick={() =>handleClick(4)}  />
+        <Square value={squares[5]} onSquareClick={() =>handleClick(5)}  />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() =>handleClick(6)}  />
+        <Square value={squares[7]} onSquareClick={() =>handleClick(7)}  />
+        <Square value={squares[8]} onSquareClick={() =>handleClick(8)}  />
       </div>
     </>
   );
 }
 
-function Square({value}) {
-  // const [value, setValue] = useState(null);
-  // function handleClick() {
-  //   if (value==null) {
-  //     setValue('X');
-  //   } else if (value=='X') {
-  //     setValue('O')
-  //   } else {
-  //     setValue(null)
-  //   }
-  // }
+function Square({ value, onSquareClick, i }) {
+
 
   return (
-    <button
-      className="square"
-      // onClick={handleClick}
-    >
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
